97eb0d02922dbb4af6c89778b2f41125f2bfd08d
Richard Naik <richardnaik@gmail.com>
2022-07-24 15:38:50 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,62 @@
+# Create public IP
+resource "azurerm_public_ip" "public_ip" {
+  count = length(var.pawprints)
+  name                = "${var.pawprints[count.index]}_public_ip"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Dynamic"
+}
+
+# Create network interface
+resource "azurerm_network_interface" "nic" {
+  count = length(var.pawprints)
+  name                = "${var.pawprints[count.index]}_nic"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "${var.pawprints[count.index]}_nic_config"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip[count.index].id
+  }
+}
+
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "nic_group" {
+  count = length(var.pawprints)
+  network_interface_id      = azurerm_network_interface.nic[count.index].id
+  network_security_group_id = var.security_group_id
+}
+
+# Create virtual machine
+resource "azurerm_linux_virtual_machine" "virtual_machines" {
+  count = length(var.pawprints)
+  name                  = "${var.pawprints[count.index]}"
+  location              = var.resource_group_location
+  resource_group_name   = var.resource_group_name
+  network_interface_ids = [azurerm_network_interface.nic[count.index].id]
+  size                  = "Standard_B1ls"
+
+  os_disk {
+    name                 = "${var.pawprints[count.index]}_root_disk"
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  admin_ssh_key {
+    username   = "it3850"
+    public_key = var.public_key
+  }
+
+  computer_name  = "${var.pawprints[count.index]}"
+  admin_username = var.admin_username
+  disable_password_authentication = true
+}
---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "pawprints" {
+  description = "student identifiers"
+  type = list
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "resource_group_location" {
+  type = string
+}
+
+variable "virtual_network" {
+  type = string
+}
+
+variable "security_group_id" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "public_key" {
+  type = string
+}
+
+variable "admin_username" {
+  type = string
+}
